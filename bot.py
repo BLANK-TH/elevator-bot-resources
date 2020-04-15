56992ddd27d1dd8721a8508ed90083e5ef7448f4
Theodore Hua <theodorehua8a@gmail.com>
2020-04-15 09:30:39 -0400

    Spam and FastSpam Command (Me Only) Version 13.33.61

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ import arrow
 import typing
 
 client = commands.Bot(command_prefix = 's!')
-df = "Elevator Server Bot Ver.13.32.61 Developed By: Kanade Tachibana"
+df = "Elevator Server Bot Ver.13.33.61 Developed By: Kanade Tachibana"
 game = cycle(["A Bot for the Elevator Discord Server!",'Developed By: Kanade Tachibana','STFU Pokecord with your annoying level up messages!','Use s!help to see my commands!',df.replace(" Developed By: Kanade Tachibana","")])
 hc = 0x8681bb
 client.remove_command('help')
@@ -1587,5 +1587,52 @@ async def _purge(ctx,number="None"):
             embed.set_footer(text=df)
     message = await ctx.message.channel.send(embed=embed)
     await message.delete(delay=7)
+
+@client.command()
+async def spam(ctx,num:int,*,message):
+    if not message.author.id == 616032766974361640:
+        msg = await ctx.message.channel.send("The spam command can only be used by the bot owner a.k.a. **NOT YOU**")
+        await msg.delete(delay=30)
+        return
+    await ctx.message.delete()
+    log_channel = client.get_channel(681612436293419021)
+    embed = discord.Embed(
+        title="Someone used the spam command!",
+        colour=hc
+    )
+    embed.add_field(name="Command Author Name:", value=ctx.message.author.display_name)
+    embed.add_field(name="Command Author ID:", value=ctx.message.author.id)
+    embed.add_field(name="Channel Used In:", value=ctx.message.channel.name)
+    embed.add_field(name="Number of Messages:", value=str(num))
+    embed.add_field(name="Message:", value=message)
+    embed.set_footer(text=df)
+    await log_channel.send(embed=embed)
+
+    for x in range(0,num):
+        await ctx.message.channel.send(message)
+        await asyncio.sleep(0.9)
+
+@client.command()
+async def fastspam(ctx,num:int,*,message):
+    if not message.author.id == 616032766974361640:
+        msg = await ctx.message.channel.send("The spam command can only be used by the bot owner a.k.a. **NOT YOU**")
+        await msg.delete(delay=30)
+        return
+    await ctx.message.delete()
+    log_channel = client.get_channel(681612436293419021)
+    embed = discord.Embed(
+        title="Someone used the spam command!",
+        colour=hc
+    )
+    embed.add_field(name="Command Author Name:", value=ctx.message.author.display_name)
+    embed.add_field(name="Command Author ID:", value=ctx.message.author.id)
+    embed.add_field(name="Channel Used In:", value=ctx.message.channel.name)
+    embed.add_field(name="Number of Messages:", value=str(num))
+    embed.add_field(name="Message:", value=message)
+    embed.set_footer(text=df)
+    await log_channel.send(embed=embed)
+
+    for x in range(0,num):
+        await ctx.message.channel.send(message)
 
 client.run('Njk5Njc3MTA4NjA3MTIzNTQ4.XpX3HQ.hIfoh4Q6KzH52D25KYR-QGNMl8k')
