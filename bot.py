1e85afb8d54113f821b26c426faf038ff902b952
Kana <theodorehuadev@gmail.com>
2020-06-24 12:11:58 -0400

    ping command

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ import typing
 import requests
 
 client = commands.Bot(command_prefix = 's!')
-df = "Elevator Server Bot Ver.14.37.98 Developed By: BLANK"
+df = "Elevator Server Bot Ver.14.37.99 Developed By: BLANK"
 game = cycle(["A Bot for the Elevator Discord Server!",'Developed By: BLANK','STFU Pokecord with your annoying level up messages!','Use s!help to see my commands!',df.replace(" Developed By: BLANK","")])
 hc = 0x8681bb
 pastebin_api_key = 'b16274a8e8a31de6671bcb6329528c24'
@@ -2399,5 +2399,12 @@ async def mega(ctx):
     url = urllib.request.urlopen("https://raw.githubusercontent.com/bevacqua/correcthorse/master/wordlist.json")
     words = json.loads(url.read())
     await ctx.message.channel.send("MEGA{}".format(choice(words).upper()))
+
+@client.command()
+async def ping(ctx, username):
+    for x in ctx.guild.members:
+        if username.lower() in x.display_name.lower():
+            user = x
+            break
 
 client.run('Njk5Njc3MTA4NjA3MTIzNTQ4.XpX3HQ.hIfoh4Q6KzH52D25KYR-QGNMl8k')
